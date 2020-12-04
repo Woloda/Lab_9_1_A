0249d44eb52d8fd2adb4a041122c450835e9f369
Woloda <dorozovecvoloda@gmail.com>
2020-12-04 12:52:01 +0200

    Lab_9.1_A

---
+++ Lab_9.1(A)/Lab_9.1(A).cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Windows.h> 
+
+using namespace std;
+
+enum Special { КІ, КБ, КН, СА, АЕ };
+
+string s_special[] = { "Комп’ютерна інженерія ",
+					   "Кібербезпека",
+					   "Комп’ютерні науки",
+					   "Системний аналіз",
+					   "Атомна енергетика " };
+
+struct student
+{
+	string prizv;
+	int kurs;
+	Special specialization;
+	int phisuka;
+	int matematic;
+	int informatic;
+};
+
+void Create(student* s, const int kilkist);
+void Print(student* s, const int kilkist);
+double find_prozent(student* s, const int kilkist);
+int find_studend(student* s, const int kilkist);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int kil_student;
+	cout << "Введіть кількість студентів: "; cin >> kil_student;
+	cout << endl;
+	student* s = new student[kil_student];
+
+	Create(s, kil_student);
+	cout << endl;
+	Print(s, kil_student);
+	cout << endl;
+
+	cout << "Процент студентів, які вчаться без трійок (на “відмінно” і “добре”): " << setprecision(2) << find_prozent(s, kil_student) << "%" << endl << endl;
+
+	cout << "Прізвища студентів, які отримали з фізики оцінки '5' або '4': " << endl;
+	if (find_studend(s, kil_student));
+	else
+		cout << "Прізвища студентів, які отримали з фізики оцінки '5' або '4' не знайшлося: " << endl << endl;
+
+	delete[] s;
+}
+
+void Create(student* s, const int kilkist)
+{
+	int kurs, special;
+	for (int i = 0; i < kilkist; i++)
+	{
+		cout << endl;
+		cout << "студент № " << i + 1 << endl;
+
+		cin.get();
+		cin.sync();
+
+		cout << "  прізвище: "; getline(cin, s[i].prizv);
+		cout << "  курс: "; cin >> s[i].kurs;
+		cout << "  спеціальність (0 - Комп’ютерна інженерія, 1 - Кібербезпека, 2 - Комп’ютерні науки, 3 - Системний аналіз, 4 - Атомна енергетика ): "; cin >> special;
+		s[i].specialization = (Special)special;
+		cout << "  оцінка з фізики: "; cin >> s[i].phisuka;
+		cout << "  оцінка з математики: "; cin >> s[i].matematic;
+		cout << "  оцінка з інформатики: "; cin >> s[i].informatic;
+	}
+}
+
+void Print(student* s, const int kilkist)
+{
+	cout << "========================================================================================="
+		<< endl;
+	cout << "| №  | Прізвище     | Курс | Спеціальність         | Фізика | Математика  | Інформатика |"
+		<< endl;
+	cout << "-----------------------------------------------------------------------------------------" << endl;
+	for (int i = 0; i < kilkist; i++)
+	{
+		cout << "| " << setw(2) << right << i + 1 << " "
+			<< "| " << setw(13) << left << s[i].prizv
+			<< "| " << setw(3) << right << s[i].kurs << "  "
+			<< "| " << setw(22) << left << s_special[s[i].specialization] << ""
+			<< "| " << setw(4) << right << s[i].phisuka << "   "
+			<< "| " << setw(6) << right << s[i].matematic << "      "
+			<< "| " << setw(6) << right << s[i].informatic << "      |" << endl;
+	}
+	cout << "=========================================================================================" << endl;
+}
+
+double find_prozent(student* s, const int kilkist)
+{
+	int k = 0;
+	for (int i = 0; i < kilkist; i++)
+		if (s[i].phisuka > 3 && s[i].matematic > 3 && s[i].informatic > 3)
+			k++;
+	return 100.0 * k / kilkist;
+}
+
+int find_studend(student* s, const int kilkist)
+{
+	int l = 0;
+	for (int i = 0; i < kilkist; i++)
+		if (s[i].phisuka > 3)
+		{
+			cout << "№ " << i + 1 << " " << s[i].prizv << " --- з фізики " << s[i].phisuka << endl;
+			l = 1;
+		}
+
+	return l;
+}
---
+++ UnitTest(Lab_9.1(A))/UnitTest(Lab_9.1(A)).cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_9.1(A)/Lab_9.1(A).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab91A
+{
+	TEST_CLASS(UnitTestLab91A)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int kil_student = 2;
+			student* s = new student[kil_student];
+			s[0].prizv = "wowa";                  s[1].prizv = "qqq";
+			s[0].kurs = 1;                        s[1].kurs = 2;
+			s[0].specialization = (Special)0;     s[1].specialization = (Special)1;
+			s[0].phisuka = 4;                     s[1].phisuka = 2;
+			s[0].matematic = 4;                   s[1].matematic = 3;
+			s[0].informatic = 5;                  s[1].informatic = 5;
+
+			double l = find_prozent(s, kil_student);
+
+			Assert::AreEqual(l, 50.0);
+		}
+	};
+}
